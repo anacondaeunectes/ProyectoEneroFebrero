ad2d5aafd390306930a7a61bae9099e4132aba7b
AlvaroBarrasa <56679932+AlvaroBarrasa@users.noreply.github.com>
2020-02-06 19:26:27 +0100

    Add files via upload

---
+++ Videoclub.Alvaro/src/Videoclub/Main.java
@@ -0,0 +1,17 @@
+package Videoclub;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		FuncionPelicula p = new FuncionPelicula();
+		p.addPelicula();
+		p.listarPelicula();
+	}
+	
+	public void alquilar() {
+		
+	}
+}
